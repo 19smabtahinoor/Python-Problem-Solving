620dc87c78e58ba982e82d44a3d7dc67275d2d08
S.M.ABTAHI NOOR <>
2021-06-06 11:11:54 +0000

    Floor

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import random
+import math
 
-random_number = random.randint(0,20)
-print(random_number)
+num1 = int(input('Write 1st number'))
+num2 = int(input('Write 2nd number'))
+
+result = num1 // num2 
+print(result)
+
+# another way
+get_Floor_number_1 = math.floor(3.4)
+print(get_Floor_number_1)
