91ec662ea1276e51b06ce0164d31b041fa59cdd7
S.M.ABTAHI NOOR <>
2021-06-06 13:15:09 +0000

    Swap Variables

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-import math
 
-num1 = int(input('Write 1st number'))
-num2 = int(input('Write 2nd number'))
+# Task is swap two variables
 
-result = num1 // num2 
-print(result)
+# solution 1 
+print('Solution 1')
+a = 5
+b = 10
 
-# another way
-get_Floor_number_1 = math.floor(3.4)
-print(get_Floor_number_1)
+temp = a
+a = b
+b = temp
+print('a,b => ',a,b)
+
+
+# soluton 2
+print('Solution 2')
+a = 12
+b = 24
+
+a,b = b,a
+print('a,b',a,b)
+
+
+# Solution 3
+print('Solution 3')
+a = 20
+b = 40 
+
+a = a + b
+b = a - b
+a = a - b
+print('a,b',a,b)
