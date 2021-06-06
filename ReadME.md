d16196a06ce9a37c0153607e70ea8bf3e4e14125
S.M.ABTAHI NOOR <>
2021-06-06 10:19:32 +0000

    1st Solving Done

---
+++ ReadME.md
@@ -0,0 +1,1 @@
+# Problem Solving with Python
---
+++ main.py
@@ -0,0 +1,6 @@
+intNumber = int(input('Write a first Number : '))
+floatNumber = float(input('Write a second Number : '))
+
+totalInput = intNumber * floatNumber
+print("Your result is ",totalInput)
+
