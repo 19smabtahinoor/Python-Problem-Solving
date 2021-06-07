8f05d45f2ee66fd92a82a38a6fe7a1124aa50163
S.M.ABTAHI NOOR <>
2021-06-07 14:59:23 +0000

    another solution

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# solution 1 
 def get_sums(nums):
   sum = 0
   for num in nums:
@@ -7,3 +9,9 @@ def get_sums(nums):
 nums = [1,2,3,4,5]
 total = get_sums(nums)
 print('Total number is',total)
+
+
+# solution 2
+nums = [1,2,3,4,5,6,7,8,9,10]
+total = sum(nums)
+print(total)
