1506666ad2f8b1ef02781ccad52b65d09d0040de
S.M.ABTAHI NOOR <>
2021-06-07 05:40:23 +0000

    Leap year

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
+# Problem : We take a input of any year.Then we can say it's leapyear or not.
 
-# Solution 1
-num1 = input("1st number : ")
-num2 = input("2nd number : ")
-num3 = input("3rd number : ")
+year = int(input("Enter a year: "))
 
-if(num1 >= num2 and num1 >= num3):
-  print("Highest Value is:", num1)
-elif(num2 >= num1 and num2 >= num3):
-  print("Hightest Value is: ",num2)
+if year%4 == 0:
+    is_leapyear = year," is a leap year"
 else:
-  print("Highest Value is: ",num3)
+    is_leapyear = year," is not a leap year"
 
-
-# Solution 2
-num1 = input("1st number : ")
-num2 = input("2nd number : ")
-num3 = input("3rd number :")
-
-print("Highest Value is: ",max(num1,num2,num3))
+print(is_leapyear)
