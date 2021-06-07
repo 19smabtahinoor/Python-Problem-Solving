32fde4722eb06e7b9e02e7c1b93aeebf47d6ff05
S.M.ABTAHI NOOR <>
2021-06-07 06:09:22 +0000

    letter count

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ count = 0
 for char in user_Text:
   if char == ' ':
     count = count + 1
-
+    
 count = count + 1
+print(count)
+
+
+# Letter count
+user_Text = input("Write your bio:")
+count = 0
+for char in user_Text:
+  count = count + 1
 print(count)
