ff1a7657596ad45ade916372cf2f475b7cceb315
S.M.ABTAHI NOOR <>
2021-06-07 06:27:49 +0000

    Vowels Count

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# Problem : Word Count
+# Problem : Vowels Count
 
-user_Text = input("Write your bio:")
-count = 0
-for char in user_Text:
-  if char == ' ':
-    count = count + 1
-    
-count = count + 1
-print(count)
-
-
-# Letter count
-user_Text = input("Write your bio:")
-count = 0
-for char in user_Text:
-  count = count + 1
-print(count)
+def count_variables(sentence):
+  vowels=['a','e','i','o','u','A','E','I','O','U']
+  count = 0
+  for char in sentence:
+    if char in vowels:
+      count = count +1
+  return count
+  
+print(count_variables(input("Write your name : ")))
